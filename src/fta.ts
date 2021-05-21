9ad00371abf1ac669dfcc2c76f0ab203afe2dad9
Dao Hoang Son <daohoangson@gmail.com>
2021-05-22 01:44:32 +0700

    Fix --import chart values

--- src/fta.ts
+++ src/fta.ts
@@ -17,7 +17,29 @@ export interface Node {
 
 export type Writer = (values: string[]) => Promise<void>
 
-const dates = ['2019-01-14', '2020-01-01', '2021-01-01', '2022-01-01', '2023-01-01', '2024-01-01', '2025-01-01', '2026-01-01', '2027-01-01', '2028-01-01', '2029-01-01', '2030-01-01', '2031-01-01', '2032-01-01', '2033-01-01', '2034-01-01']
+const dates = [
+  '2019-01-14',
+  '2020-01-01',
+  '2021-01-01',
+  '2022-01-01',
+  '2023-01-01',
+  '2024-01-01',
+  '2025-01-01',
+  '2026-01-01',
+  '2027-01-01',
+  '2028-01-01',
+  '2029-01-01',
+  '2030-01-01',
+  '2031-01-01',
+  '2032-01-01',
+  '2033-01-01',
+  '2034-01-01',
+  '2035-01-01',
+  '2036-01-01',
+  '2037-01-01',
+  '2038-01-01',
+  '2039-01-01'
+]
 export const headers = [
   'Mã',
   'Hiệp định',
@@ -108,16 +130,25 @@ async function parse (html: string): Promise<string[]> {
 
   const chartJs = (html.match(/var chartJS_graph0 = new Chart\(\$\('#graph0'\),(.+)/) ?? [])[1] ?? ''
   const chartLabels = ((chartJs.match(/"labels":\["([0-9",-]+)"\]/) ?? [])[1] ?? '').split('","')
-  if (chartLabels.length === dates.length) {
-    if (chartLabels.join(',') !== dates.join(',')) {
-      throw new Error(JSON.stringify({ chartJs, chartLabels }))
+  if (chartLabels.length > 1) {
+    if (chartLabels.length > dates.length) {
+      throw new Error(JSON.stringify({ chartJs, chartLabels, error: 'chartLabels.length > dates.length' }))
+    }
+    for (let i = 0; i < chartLabels.length; i++) {
+      if (chartLabels[i] !== dates[i]) {
+        throw new Error(JSON.stringify({ chartJs, chartLabels, i }))
+      }
     }
 
     const chartValues = ((chartJs.match(/"data":\["([0-9",.]+)"\]/) ?? [])[1] ?? '')
       .split('","')
       .map((str) => `${parseFloat(str)}%`)
-    if (chartValues.length !== dates.length) {
-      throw new Error(JSON.stringify({ chartJs, chartValues }))
+    if (chartValues.length !== chartLabels.length) {
+      throw new Error(JSON.stringify({ chartJs, chartLabels, chartValues }))
+    }
+
+    while (chartValues.length < dates.length) {
+      chartValues.push('N/A')
     }
 
     return [...texts, ...chartValues]
